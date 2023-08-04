5aee047fadc12af31c29a44ecc5f4482446884b3
Norbert Podgórski <npodgorski@student.agh.edu.pl>
2023-08-05 01:08:04 +0200

    batches and recordings changed

--- run/detect.py
+++ run/detect.py
@@ -12,9 +12,23 @@ def detect(config: Dict[str, Any]):
     frames_loader = FramesLoader(**config["frames_loader_config"])
     detector = DetectorFactory.create(**config["used_detector"])
     visualizer = Visualizer(**config["visualizer_config"])
-
     frames = frames_loader.load_all_frames()
-    detections = detector.detect(frames)
+
+    batch_size = config["batch_size"]
+    scenes_number = frames.shape[0]
+    detections = []
+    batch_low_idx = 0
+    batch_high_idx = 0
+    while batch_high_idx <= scenes_number:
+        batch_high_idx += batch_size
+        if batch_high_idx >= scenes_number:
+            batch_high_idx = scenes_number + 1
+        batch = frames[batch_low_idx: batch_high_idx]
+        batch_detections = detector.detect(batch)
+        detections.append(batch_detections)
+        batch_low_idx += batch_size
+    detections = [detection for detection_for_batch in detections for detection in detection_for_batch]
+
     visualizer.visualize(frames, detections)
 
 
